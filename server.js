e90f5890e12840f9dc025e357e89eee22e70e500
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-04-26 14:18:25 -0700

    updates

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 //serves css/js/static assets
 app.use(express.static("public"))
 const allRoutes = require("./routes")
